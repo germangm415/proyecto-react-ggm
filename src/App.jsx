6b1c945ce14919de0e6ab5db08ca89d3c0b42f9f
German Gutierrez Marin <german.gutierrez@inec.go.cr>
2023-07-12 11:51:08 -0600

    clase 6.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [gatos, setGatos] = useState();
+
+  useEffect(()=>{
+    fetch("https://cataas.com/api/cats?tags=cute")
+    .then((resp)=> resp.json())
+    .then((data)=>{
+
+      setGatos(data)
+      //console.log(data);
+    })
+    .catch(()=>{
+      console.error("La petición falló");
+    })
+
+  },[])
   
 
   return (
     <>
+      <h1>Lista Gatos</h1>
+      <ol>
+        {gatos.map(({id,owner}) => {
+          <li>ID: {id} Dueño: {owner}</li>
+        })}
+
+       
+      </ol>
       
     </>
   )
